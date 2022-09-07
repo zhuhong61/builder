983cac951c774662e8b65fdcd9e6f73b4c9a2005
zhuhong61 <hong.zhu@intel.com>
2022-09-07 05:10:41 +0000

    conda: add _GLIBCXX_USE_CXX11_ABI=1 support for linux cpu wheel
    The target of this commit is to add the support for a new linux
    cpu conda wheel file built with _GLIBCXX_USE_CXX11_ABI=1.
    
    we add a Dockerfile (conda/Dockerfile_cxx11-abi) with CentOS8 as base image to
    support CXX11_ABI=1.
    
    To build the new docker image with CXX11_ABI support, run:
    CUDA_VERSION=cpu-cxx11-abi conda/build_docker.sh
    or
    conda/build_all_docker.sh
    
    To build a linux cpu conda wheel with CXX11_ABI within this image, run:
    // the below settings are special for this image
    export DESIRED_CUDA=cpu-cxx11-abi # change from cpu for wheel name
    export GPU_ARCH_TYPE=cpu-cxx11-abi # change from cpu for build.sh
    export DOCKER_IMAGE=pytorch/conda-builder:cpu-cxx11-abi
    export DESIRED_DEVTOOLSET=cxx11-abi
    
    // the below settings are as usual
    export BINARY_ENV_FILE=/tmp/env
    export BUILDER_ROOT=/builder
    export DESIRED_PYTHON=3.7 # or 3.8, 3.9, etc.
    export IS_GHA=1
    export PACKAGE_TYPE=conda
    export PYTORCH_FINAL_PACKAGE_DIR=/artifacts
    export PYTORCH_ROOT=/pytorch
    export GITHUB_WORKSPACE=/your_path_to_workspace
    
    // the '-e DESIRED_DEVTOOLSET' below is newly added for this container,
    // others are as usual
    set -x
    mkdir -p artifacts/
    container_name=$(docker run
    -e BINARY_ENV_FILE
    -e BUILDER_ROOT
    -e DESIRED_CUDA
    -e DESIRED_PYTHON
    -e GPU_ARCH_TYPE
    -e IS_GHA
    -e PACKAGE_TYPE
    -e PYTORCH_FINAL_PACKAGE_DIR
    -e PYTORCH_ROOT
    -e DOCKER_IMAGE
    -e DESIRED_DEVTOOLSET
    --tty
    --detach
    -v "${GITHUB_WORKSPACE}/pytorch:/pytorch"
    -v "${GITHUB_WORKSPACE}/builder:/builder"
    -v "${RUNNER_TEMP}/artifacts:/artifacts"
    -w /
    "${DOCKER_IMAGE}"
    )
    
    // build conda wheel as usual,
    // and the built wheel file name looks like: pytorch-1.13.0.dev20220616-py3.7_cpu_0.tar.bz2
    docker exec -t -w "${PYTORCH_ROOT}" "${container_name}" bash -c "bash .circleci/scripts/binary_populate_env.sh"
    docker exec -t "${container_name}" bash -c "source ${BINARY_ENV_FILE} && bash /builder/conda/build.sh"
    
    // to verify the built wheel file, we'll see 'True'
    $ conda install pytorch-1.13.0.dev20220616-py3.7_cpu_0.tar.bz2
    $ python -c 'import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI)'
    True
    
    Co-authored-by: Zhu Hong hong.zhu@intel.com
    Co-authored-by: Guo Yejun yejun.guo@intel.com

--- conda/build_all_docker.sh
+++ conda/build_all_docker.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+v#!/usr/bin/env bash
 
 set -eou pipefail
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-for CUDA_VERSION in 11.8 11.7 11.6 cpu; do
+for CUDA_VERSION in 11.7 11.6 11.5 11.3 10.2 cpu cpu-cxx11-abi; do
   CUDA_VERSION="${CUDA_VERSION}" conda/build_docker.sh
 done
